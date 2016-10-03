a2d2683701c87ddd991e2af3264dda8ddd249e73
ilikesounds <email_id>
2016-10-02 22:50:51 -0700

    step 2 complete

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -8,18 +8,6 @@ class SnippetSerializer(serializers.ModelSerializer):
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
-    # pk = serializers.IntegerField(read_only=True)
-    # title = serializers.CharField(required=False,
-    #                               allow_blank=True,
-    #                               max_length=100
-    #                               )
-    # code = serializers.CharField(style={'base_template': 'textarea.html'})
-    # linenos = serializers.BooleanField(required=False)
-    # language = serializers.ChoiceField(choices=LANGUAGE_CHOICES,
-    #                                    default='python'
-    #                                    )
-    # style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
     def create(self, validated_data):
         """
         Create and return a new `Snippet` instance, given the validated data.
