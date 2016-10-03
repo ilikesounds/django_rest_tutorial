f84d5b7e147d376fa6fe416a19ee14cdf7cf074a
ilikesounds <email_id>
2016-10-03 00:52:19 -0700

    auth login added

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,9 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializer import UserSerializer
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -11,6 +14,10 @@ class SnippetList(generics.ListCreateAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -20,3 +27,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
